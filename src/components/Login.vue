da72d5094f3f5d900f564942ba5bb90cba8f1062
JhonVilcarana <jhonvilcarana@upeu.edu.pe>
2021-07-18 01:11:15 -0500

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,68 @@
+<template>
+     <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <title>Contact Form Template</title>
+        <link rel="stylesheet" href="/build/tailwind.css" type="text/css" media="screen" title="no title" charset="utf-8" />
+    </head>
+    <body class>
+        <div class="flex justify-center">
+            <div class="lg:w-1/2 xl:max-w-screen-sm">
+                <div class="py-12 bg-red-100 lg:bg-white flex justify-center lg:justify-start lg:px-12">
+                    <div class="cursor-pointer flex items-center">
+                        <div>
+                            <img src="imgs/upeu.png" alt="logo">
+                            
+                        </div>
+                        <div class="text-2xl text-red-800 tracking-wide ml-2 font-semibold">blockify</div>
+                    </div>
+                </div>
+                <div class="mt-10 px-12 sm:px-24 md:px-48 lg:px-12 lg:mt-16 xl:px-24 xl:max-w-2xl">
+                    <h2 class="text-center text-4xl text-indigo-900 font-display font-semibold lg:text-left xl:text-5xl
+                    xl:text-bold">Log in</h2>
+                    <div class="mt-12">
+                        <form>
+                            <div>
+                                <div class="text-sm font-bold text-gray-700 tracking-wide">Email Address</div>
+                                <input class="w-full text-lg py-2 border-b border-gray-300 focus:outline-none focus:border-indigo-500" type="" placeholder="mike@gmail.com">
+                            </div>
+                            <div class="mt-8">
+                                <div class="flex justify-between items-center">
+                                    <div class="text-sm font-bold text-gray-700 tracking-wide">
+                                        Password
+                                    </div>
+                                    <div>
+                                        <a class="text-xs font-display font-semibold text-indigo-600 hover:text-indigo-800
+                                        cursor-pointer">
+                                            Forgot Password?
+                                        </a>
+                                    </div>
+                                </div>
+                                <input class="w-full text-lg py-2 border-b border-gray-300 focus:outline-none focus:border-indigo-500" type="" placeholder="Enter your password">
+                            </div>
+                            <div class="mt-10" >
+                                <button class="bg-red-500 text-gray-100 p-4 w-full rounded-full tracking-wide
+                                font-semibold font-display focus:outline-none focus:shadow-outline hover:bg-red-600
+                                shadow-lg">
+                                    Log In
+                                </button>
+                            </div>
+                        </form>
+                        <div class="mt-12 text-sm font-display font-semibold text-gray-700 text-center">
+                            Don't have an account ? <a class="cursor-pointer text-indigo-600 hover:text-indigo-800">Sign up</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          
+        </div>
+    </body>
+</template>
+<script>
+</script>
+<style scoped>
+.st0{fill:none;stroke:currentColor;stroke-width:20;stroke-linecap:round;stroke-miterlimit:3;}
+.body{
+    margin: 0 auto;
+}
+</style>
